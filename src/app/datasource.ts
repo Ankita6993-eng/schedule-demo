1a281d12dca10ec60603cd7f57e9243e3e15ae57
Ankita6993-eng <ankitaa.tagline@gmail.com>
2021-05-04 12:24:54 +0530

    first

--- src/app/datasource.ts
+++ src/app/datasource.ts
@@ -2,6 +2,7 @@ export let Datasource: Object[] = [
     {
         Id: 1,
         Name: 'Meet',
+        //Appointment_time:{ StartTime: new Date(2018, 7, 1, 9, 0),EndTime: new Date(2018, 7, 1, 10, 0),},
         StartTime: new Date(2018, 7, 1, 9, 0),
         EndTime: new Date(2018, 7, 1, 10, 0),
         Description: 'Health Checkup',
@@ -9,6 +10,7 @@ export let Datasource: Object[] = [
     }, {
         Id: 2,
         Name:'Neha',
+        //Appointment_time:{ StartTime: new Date(2018, 7, 1, 10, 30),EndTime: new Date(2018, 7, 1, 11, 30),},
         StartTime: new Date(2018, 7, 1, 10, 30),
         EndTime: new Date(2018, 7, 1, 11, 30),
         Description: 'Tooth Erosion',
@@ -16,6 +18,7 @@ export let Datasource: Object[] = [
     }, {
         Id: 3,
         Name:'Neel',
+        // Appointment_time:{ StartTime: new Date(2018, 7, 1, 12, 0),EndTime: new Date(2018, 7, 1, 13, 0),},
         StartTime: new Date(2018, 7, 1, 12, 0),
         EndTime: new Date(2018, 7, 1, 13, 0),
         Description: 'Eye and Spectacles Checkup',
@@ -23,8 +26,18 @@ export let Datasource: Object[] = [
     }, {
         Id: 4,
         Name:'Mina',
+        //Appointment_time:{ StartTime: new Date(2018, 7, 1, 10, 0),EndTime: new Date(2018, 7, 1, 11, 0),},
         StartTime: new Date(2018, 7, 1, 10, 0),
         EndTime: new Date(2018, 7, 1, 11, 0),
+        Description: 'Surgery Appointment',
+        DepartmentName: 'GENERAL'
+    },
+    {
+        Id: 4,
+        Name:'Nikita',
+        //Appointment_time:{ StartTime: new Date(2018, 7, 1, 10, 0),EndTime: new Date(2018, 7, 1, 11, 0),},
+        StartTime: new Date(2018, 7, 2, 10, 0),
+        EndTime: new Date(2018, 7, 2, 11, 0),
         Description: 'Surgery Appointment',
         DepartmentName: 'GENERAL'
     }
