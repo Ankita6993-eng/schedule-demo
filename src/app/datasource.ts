020f7ab7d92a9e8db4c25f4b09062617b69d3ea9
mac <mac@macs-iMac.local>
2021-06-05 10:12:27 +0530

    first commit

--- src/app/datasource.ts
+++ src/app/datasource.ts
@@ -2,7 +2,6 @@ export let Datasource: Object[] = [
     {
         Id: 1,
         Name: 'Meet',
-        //Appointment_time:{ StartTime: new Date(2018, 7, 1, 9, 0),EndTime: new Date(2018, 7, 1, 10, 0),},
         StartTime: new Date(2018, 7, 1, 9, 0),
         EndTime: new Date(2018, 7, 1, 10, 0),
         Description: 'Health Checkup',
@@ -10,21 +9,18 @@ export let Datasource: Object[] = [
     }, {
         Id: 2,
         Name:'Neha',
-        //Appointment_time:{ StartTime: new Date(2018, 7, 1, 10, 30),EndTime: new Date(2018, 7, 1, 11, 30),},
         StartTime: new Date(2018, 7, 1, 10, 30),
         EndTime: new Date(2018, 7, 1, 11, 30),
         Description: 'Tooth Erosion'
     }, {
         Id: 3,
         Name:'Neel',
-        // Appointment_time:{ StartTime: new Date(2018, 7, 1, 12, 0),EndTime: new Date(2018, 7, 1, 13, 0),},
         StartTime: new Date(2018, 7, 1, 12, 0),
         EndTime: new Date(2018, 7, 1, 13, 0),
         Description: 'Eye and Spectacles Checkup'
     }, {
         Id: 4,
         Name:'Mina',
-        //Appointment_time:{ StartTime: new Date(2018, 7, 1, 10, 0),EndTime: new Date(2018, 7, 1, 11, 0),},
         StartTime: new Date(2018, 7, 1, 10, 0),
         EndTime: new Date(2018, 7, 1, 11, 0),
         Description: 'Surgery Appointment'
@@ -32,7 +28,6 @@ export let Datasource: Object[] = [
     {
         Id: 5,
         Name:'Nikita',
-        //Appointment_time:{ StartTime: new Date(2018, 7, 1, 10, 0),EndTime: new Date(2018, 7, 1, 11, 0),},
         StartTime: new Date(2018, 7, 2, 10, 0),
         EndTime: new Date(2018, 7, 2, 11, 0),
         Description: 'Surgery Appointment'
@@ -40,7 +35,6 @@ export let Datasource: Object[] = [
     {
         Id: 5,
         Name:'Nina',
-        //Appointment_time:{ StartTime: new Date(2018, 7, 1, 10, 0),EndTime: new Date(2018, 7, 1, 11, 0),},
         StartTime: new Date(2018, 7, 4, 11, 0),
         EndTime: new Date(2018, 7, 4, 12, 0),
         Description: 'Eye and Spectacles Checkup'
